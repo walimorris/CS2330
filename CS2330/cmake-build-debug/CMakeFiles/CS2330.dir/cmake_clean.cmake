5b31d30f09c4416db4fa049548cb7a149c637222
Wali Morris <48896622+walimorris@users.noreply.github.com>
2020-09-15 01:27:25 -0700

    Add files via upload

---
+++ CS2330/cmake-build-debug/CMakeFiles/CS2330.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CS2330.dir/main.cpp.o"
+  "CS2330"
+  "CS2330.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CS2330.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CS2330/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+/*
+ * CLion setup for CS2330 and basic program for testing
+ * Wali Morris
+ * 09/15/2020
+ */
+int main() {
+    string userName;
+    int userAge;
+    string userBirthDate;
+    cout << "Welcome to my story." << endl;
+    cout << "please enter your name: ";
+    cin >> userName;
+    cout << "Nice to meet you " << userName << ", thanks for coming." << endl;
+    cout << "Please enter your age: ";
+    cin >> userAge;
+    cout << "Please enter your birthdate in format mm/dd/year: ";
+    cin >> userBirthDate;
+    while ( userBirthDate.length() > 10 || userBirthDate.length() < 10 ) {
+        cout << "Sorry this is the wrong birthday format." << endl;
+        cout << "Please enter your birthdate in format mm/dd/year: ";
+        cin >> userBirthDate;
+    }
+    cout << "Great thank you!" << endl;
+    cout << "You were born " << userBirthDate << " and you are " << userAge << " years old." << endl;
+    return 0;
+}
